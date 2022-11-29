4c87b87cbd7d4dbbeb74db77f235fc5de5eb34e0
Tommy Klæstad <97949728+Tomklae@users.noreply.github.com>
2022-11-29 13:34:02 +0100

    first commit

---
+++ Random.py
@@ -0,0 +1,16 @@
+import random
+class randomintgen:
+
+    def gen(self,amount,minRange,maxRange):
+        for item in amount,minRange,maxRange:
+        	if not isinstance(item, int) :
+		        raise TypeError('Please make sure all inputs are integers')
+        if amount > 100:
+            raise TypeError("Please lower your amount")
+        result=random.choices(range(minRange,maxRange), weights = None, k = amount)
+        if isinstance(result,list):
+            if len(result)==amount:
+                return result 
+        else:
+            raise TypeError("Unexpected output format")
+
